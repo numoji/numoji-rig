efa58454afdcc683071a9c7ad64b74bce3d037d1
zoebasil <dog456pop@gmail.com>
2024-07-25 19:46:11 -0600

    Fix addon version

--- src/__init__.py
+++ src/__init__.py
@@ -7,7 +7,7 @@ bl_info = {
     "description": "UI Panel for managing Numoji's roblox character rig.",
     "author": "CrossStarCross @ Numoji",
     "blender": (4, 0, 0),
-    "version": (1, 1, 420),
+    "version": (1, 0, 0),
     "doc_url": "https://www.notion.so/notthatnda/Rig-UI-Documentation-ce181010f51e4d4c8ff1308ad884ed7e",
     "tracker_url": "https://www.notion.so/notthatnda/Features-Roadmap-and-Development-db480723be7a47da82d9f8371e2c5f92",
     "support": "COMMUNITY",
