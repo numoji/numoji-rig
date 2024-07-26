19ff9a95336181f4c3b05b629090760e72ff2585
zoebasil <dog456pop@gmail.com>
2024-07-25 19:47:21 -0600

    fix doc url

--- src/__init__.py
+++ src/__init__.py
@@ -8,8 +8,7 @@ bl_info = {
     "author": "CrossStarCross @ Numoji",
     "blender": (4, 0, 0),
     "version": (1, 0, 0),
-    "doc_url": "https://www.notion.so/notthatnda/Rig-UI-Documentation-ce181010f51e4d4c8ff1308ad884ed7e",
-    "tracker_url": "https://www.notion.so/notthatnda/Features-Roadmap-and-Development-db480723be7a47da82d9f8371e2c5f92",
+    "doc_url": "https://github.com/numoji/numoji-rig",
     "support": "COMMUNITY",
     "location": "View3D > Sidebar",
 }
